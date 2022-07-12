f350c19ccb64a39e9e7e0806a9585acd8d47be60
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-07-12 22:35:40 +0530

    Create PairSum.cpp

---
+++ Arrays/Mixed Problems/PairSum.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+vector<vector<int>> pairSum(vector<int> &arr, int s){
+    int n = arr.size();
+    sort(arr.begin(),arr.end());
+    vector<vector<int>> ans;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]+arr[j] == s){
+
+                    ans.push_back({arr[i],arr[j]});
+         
+            }
+        }
+
+     
+    }
+       return ans;
+}
